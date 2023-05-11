ed3435f981cad2e4a657942da8291ef17dedabb5
Anthony Marion <anthonycmarion@gmail.com>
2023-05-11 19:49:54 +1000

    Added create method to controller and added create repo form view
    This will allow the user to create repos from the app then push it to github with a push request

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,4 +3,18 @@ class RepositoriesController < ApplicationController
     client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
     @repos = client.repos.sort_by { |repo| repo.created_at }.reverse
   end
+
+  def create
+    client = Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+    repo_name = params[:name]
+
+    begin
+      client.create_repository(repo_name, options = {})
+      flash[:success] = "Repository '#{repo_name}' created."
+    rescue Octokit::UnprocessableEntity => e
+      flash[:error] = e.message
+    end
+
+    redirect_to root_path
+  end
 end
